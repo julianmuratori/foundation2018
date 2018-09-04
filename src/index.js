f86a69f75dce7a750e55d5834083f467569701ed
Julian Muratori <julianmuratori@gmail.com>
2018-09-04 16:51:41 -0400

    styling financials modal

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 
 
 // import "./scss/main.scss";
+import Siema from 'siema';
 require('../src/scss/main.scss');
 
+
+// variables for modal functionality 
 const sfap = document.getElementById('sfap')
 const march = document.getElementById('march')
 const financials = document.getElementById('financials')
+
+
+// *** MODAL COMBAT *** 
 
 // function to add is-active to the sfap modal
 function sfapModal(e) {    
@@ -43,6 +49,33 @@ sfap.addEventListener("click", sfapModal, false);
 march.addEventListener("click", marchModal, false);
 financials.addEventListener("click", financialsModal, false);
 
+// *** FINISH HIM ***
 
+const highlights = new Siema({
+    draggable: false 
+})
+
+const pac = document.getElementById('pac')
+const teenPhilanthropy = document.getElementById("teen-philanthropy");
+const legacy = document.getElementById("legacy");
+const bol = document.getElementById("bol");
+
+
+legacy.addEventListener("click", e => {
+    e.preventDefault();
+    highlights.goTo(0);
+})
+pac.addEventListener("click", e => {
+    e.preventDefault();
+    highlights.goTo(1);
+})
+bol.addEventListener('click', (e) => {
+e.preventDefault();
+highlights.goTo(2);
+})
+teenPhilanthropy.addEventListener("click", e => {
+  e.preventDefault();
+  highlights.goTo(3);
+});
 
 
